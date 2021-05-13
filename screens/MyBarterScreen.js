3da68b52e72814b5e035099edd2d53e4c0ff022f
StarKingFahim <fahimlegendarycraft@gmail.com>
2021-05-13 21:34:33 +0530

    Notifications And Screens

---
+++ screens/MyBarterScreen.js
@@ -0,0 +1,127 @@
+import React ,{Component} from 'react'
+import {View, Text,TouchableOpacity,ScrollView,FlatList,StyleSheet} from 'react-native';
+import {Card,Icon,ListItem} from 'react-native-elements'
+import firebase from 'firebase';
+import db from '../Config.js'
+
+export default class MyBartersScreen extends Component {
+  static navigationOptions = { header: null };
+
+   constructor(){
+     super()
+     this.state = {
+       userId : firebase.auth().currentUser.email,
+       allBarters : []
+     }
+     this.requestRef= null
+   }
+
+
+   getAllBarters =()=>{
+     this.requestRef = db.collection("all_Barters").where("donor_id" ,'==', this.state.userId)
+     .onSnapshot((snapshot)=>{
+       var allBarters = snapshot.docs.map(document => document.data());
+       this.setState({
+         allBarters : allBarters,
+       });
+     })
+   }
+
+   keyExtractor = (item, index) => index.toString()
+
+   renderItem = ( {item, i} ) =>(
+     <ListItem
+       key={i}
+       title={item.item_name}
+       subtitle={"Requested By : " + item.requested_by +"\nStatus : " + item.request_status}
+       leftElement={<Icon name="book" type="font-awesome" color ='#696969'/>}
+       titleStyle={{ color: 'black', fontWeight: 'bold' }}
+       rightElement={
+           <TouchableOpacity style={styles.button}>
+             <Text style={{color:'#ffff'}}>Exchange</Text>
+           </TouchableOpacity>
+         }
+       bottomDivider
+     />
+   )
+
+   sendNotification=(bookDetails,requestStatus)=>{
+     var requestId = bookDetails.request_id
+     var donorId = bookDetails.donor_id
+     db.collection("all_notifications")
+     .where("request_id","==", requestId)
+     .where("donor_id","==",donorId)
+     .get()
+     .then((snapshot)=>{
+       snapshot.forEach((doc) => {
+         var message = ""
+         if(requestStatus === "Book Sent"){
+           message = this.state.donorName + " sent you book"
+         }else{
+            message =  this.state.donorName  + " has shown interest in donating the book"
+         }
+         db.collection("all_notifications").doc(doc.id).update({
+           "message": message,
+           "notification_status" : "unread",
+           "date"                : firebase.firestore.FieldValue.serverTimestamp()
+         })
+       });
+     })
+   }
+
+
+   componentDidMount(){
+     this.getAllBarters()
+   }
+
+   componentWillUnmount(){
+     this.requestRef();
+   }
+
+   render(){
+     return(
+       <View style={{flex:1}}>
+         <View style={{flex:1}}>
+           {
+             this.state.allBarters.length === 0
+             ?(
+               <View style={styles.subtitle}>
+                 <Text style={{ fontSize: 20}}>List of all Barters</Text>
+               </View>
+             )
+             :(
+               <FlatList
+                 keyExtractor={this.keyExtractor}
+                 data={this.state.allBarters}
+                 renderItem={this.renderItem}
+               />
+             )
+           }
+         </View>
+       </View>
+     )
+   }
+   }
+
+
+const styles = StyleSheet.create({
+  button:{
+    width:100,
+    height:30,
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor:"#ff5722",
+    shadowColor: "#000",
+    shadowOffset: {
+       width: 0,
+       height: 8
+     },
+    elevation : 16
+  },
+  subtitle :{
+    flex:1,
+    fontSize: 20,
+    justifyContent:'center',
+    alignItems:'center'
+  }
+})
